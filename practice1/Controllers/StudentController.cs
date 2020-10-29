1e898b61efa52427c3a9ea3faecd7942ed923d10
MVCSampleCode <73609209+MVCSampleCode@users.noreply.github.com>
2020-10-30 00:36:09 +0530

    Add files via upload

---
+++ practice1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using practice1.Models;
+namespace practice1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student stud = new Student();
+            stud.StudentId = 1;
+            stud.StudentName = "Sagar";
+            stud.isNewlyEnrolled = true;
+            stud.Gender = "Male";
+            stud.Grade = "A+";
+            return View(stud);
+        }
+    }
+}
