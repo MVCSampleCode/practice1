ae8155396caeb751c999509b379491ad182d5c06
MVCSampleCode <73609209+MVCSampleCode@users.noreply.github.com>
2020-10-30 00:35:28 +0530

    Add files via upload

---
+++ practice1/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace practice1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [DisplayName("Name")]
+        public string StudentName { get; set; }
+
+        public string Gender { get; set; }
+
+        [DisplayName("NewlyEnrolled ")]
+        public bool isNewlyEnrolled { get; set; }
+
+        public string Grade { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
